2ed6b811cc727f0af65d5d79441b31986bba6f9b
Will Schuetz <willschuetz@outlook.com>
2018-10-14 16:01:45 +1100

    Reads in bytes from umbilical

--- source/drivers/umbilical.cpp
+++ source/drivers/umbilical.cpp
@@ -1,16 +1,49 @@
 #include <source/drivers/payload.h>
 #include <source/drivers/umbilical.h>
+#include <string.h>
 #include <ti/devices/msp432e4/driverlib/driverlib.h>
+#include <source/globals.h>
 
-#define UMBILICAL_UART UART1_BASE
+uint8_t umbilical_buffer[UMBILICAL_BUFFER_MAX_LEN];
+uint8_t umbilical_buffer_len;
 
-void UART1_IRQHandler(void) {
-    // TODO
+extern "C" {
+    void UART1_IRQHandler(void) {
+        uint32_t ui32Status = MAP_UARTIntStatus(UMBILICAL_UART, true);
+        MAP_UARTIntClear(UMBILICAL_UART, ui32Status);
+        SYSTEM_FLAGS |= kUmbilicalReadBitMask;
+    }
 }
 
-err_t validateUmbilicalPacket(uint8_t* buffer, uint8_t buffer_len) {
+err_t umbilicalRead() {
+    uint32_t counter = 0;
+
+    // Read in the sync and size bytes
+    while (counter < sizeof(uint8_t) * 3) {
+        umbilical_buffer[counter] = (uint8_t)MAP_UARTCharGet(UMBILICAL_UART);
+        counter++;
+    }
+
+    err_t validated_header = validateUmbilicalHeader(umbilical_buffer, 3);
+
+    if (validated_header != UMB_NO_ERROR) {
+        return validated_header;
+    }
+
+    // Read in the rest of the packet
+    while (counter < umbilical_buffer[2]) {
+        umbilical_buffer[counter] = (uint8_t)MAP_UARTCharGet(UMBILICAL_UART);
+        counter++;
+    }
+
+    umbilical_buffer_len = counter - 1;  // To account for the final ++
+
+    return UMB_NO_ERROR;
+}
+
+err_t validateUmbilicalHeader(uint8_t* buffer, uint8_t buffer_len) {
     // Check for full header
-    if (buffer_len < 5) {
+    if (buffer_len < 3) {
         return UMB_BAD_PACKET_LENGTH;
     }
 
@@ -19,18 +52,21 @@ err_t validateUmbilicalPacket(uint8_t* buffer, uint8_t buffer_len) {
         return UMB_BAD_PACKET_HEADER;
     }
 
-    // Length of packet (excl sync chars)
-    if (buffer[2] != buffer_len) {
-        return UMB_BAD_PACKET_LENGTH;
-    }
-
-    // TODO Checksum test buffer[4]
-
-    return 0;
+    return UMB_NO_ERROR;
 }
 
 err_t getUmbilicalPacket(uint8_t* destination, uint8_t* buffer_len) {
-    // TODO
+    // Disable UART to ensure we do not get interrupted half way through copying
+    // the packet
+    MAP_UARTDisable(UMBILICAL_UART);
+
+    if (umbilical_buffer_len == 0) {
+        MAP_UARTEnable(UMBILICAL_UART);
+        return UMB_NO_PACKET;
+    }
+    memcpy(destination, umbilical_buffer, umbilical_buffer_len);
+    *buffer_len = umbilical_buffer_len;
+    MAP_UARTEnable(UMBILICAL_UART);
 
     return UMB_NO_ERROR;
 }
---
+++ source/globals.h
@@ -0,0 +1,6 @@
+#ifndef SOURCE_GLOBALS_H_
+#define SOURCE_GLOBALS_H_
+
+extern uint8_t SYSTEM_FLAGS;
+
+#endif /* SOURCE_GLOBALS_H_ */
