7ac3e9ca8c66f042e332356a3a071005a1c9c6b0
Rowan Skewes <rowan.skewes@gmail.com>
2018-11-04 14:33:11 +1100

    Reduce wait times

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main(void) {
     if (hibernate_on_boot) {
         GPIOPinWrite(sys_reset_port, low_power_trigger_pin,
                      low_power_trigger_pin);
-        SysCtlDelay(10 * system_clock_hz);  // 10 second wait before hibernating
+        SysCtlDelay(1 * system_clock_hz);  // 30 second wait before hibernating
         GPIOPinWrite(sys_reset_port, sys_reset_pin, 0x00);
         MAP_HibernateRTCMatchSet(0, (MAP_HibernateRTCGet() + hibernate_time_s));
         MAP_HibernateRequest();
